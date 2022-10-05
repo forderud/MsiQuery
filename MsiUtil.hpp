34165d0d396033bf6de1610d8f37508ef48f93bb
Fredrik Orderud <fredrik.orderud@ge.com>
2022-10-05 13:38:09 +0200

    Add early detecton of INSTALLSTATE_ABSENT.

--- MsiUtil.hpp
+++ MsiUtil.hpp
@@ -53,6 +53,8 @@ static std::wstring GetProductInfo (const std::wstring& product_code, const wcha
 static std::wstring GetComponentPath (const std::wstring& product, const std::wstring& component) {
     DWORD buf_len = 0;
     UINT ret = MsiGetComponentPath(product.c_str(), component.c_str(), nullptr, &buf_len);
+    if (ret == INSTALLSTATE_ABSENT)
+        throw std::runtime_error("MsiGetComponentPath returned INSTALLSTATE_ABSENT");
     assert(ret == INSTALLSTATE_LOCAL);
 
     std::wstring buffer(buf_len++, L'\0'); 
