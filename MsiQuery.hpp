66d34f11f05ba00760e8dc744beef0a7eb8c96b4
Fredrik Orderud <forderud@gmail.com>
2022-10-08 13:43:55 +0200

    Embed CustomAction::Type enum.

--- MsiQuery.hpp
+++ MsiQuery.hpp
@@ -10,96 +10,96 @@
 #include <MsiDefs.h>
 
 
-/** CustomAction type parser.
-*   Based on msidbCustomActionType enum in <msidefs.h>
-*   REF: https://docs.microsoft.com/en-us/windows/win32/msi/summary-list-of-all-custom-action-types
-* 
-*   Wix custom actions:
-*    Type 65   (0x041)  =                                                                Continue(0x40)                    + Dll(0x01) // ValidatePath, PrintEula
-*   Custom DLL:
-*    Type               =                                                                                                  + Dll(0x01) // Type 1/17 run DLL
-*   Custom EXE:
-*    Type 3106 (0x0C22) =                       NoImpersonate(0x800) + Deferred(0x400)                  + Directory(0x20)  + Exe(0x02) // Type 2/18/34/50 run executable
-*    Type 3170 (0x0C62) =                       NoImpersonate(0x800) + Deferred(0x400) + Continue(0x40) + Directory(0x20)  + Exe(0x02)
-*   Custom JScript:
-*    Type 7189 (0x1C15) = Script64Bit(0x1000) + NoImpersonate(0x800) + Deferred(0x400) +                  SourceFile(0x10) + Script(0x04) + Dll(0x01) // Type 5/21/37/53 JScript
-*    Type 7253 (0x1C55) = Script64Bit(0x1000) + NoImpersonate(0x800) + Deferred(0x400) + Continue(0x40) + SourceFile(0x10) + Script(0x04) + Dll(0x01)
-*   Custom VBScript:
-*    Type               =                                                                                                  + Script(0x04) + Exe(0x02) // Type 6/22/38/54 VBScript
-*
-*/
-struct CustomActionType {
-    CustomActionType () {
-        memset(this, 0, sizeof(CustomActionType)); // replace with default member initializers after upgrading to newer C++ version
-    }
-
-    /** Parse MSI CustomAction "Type" column. */
-    CustomActionType (int val) {
-        (int&)(*this) = val;
-    }
-
-    std::wstring ToString() const {
-        std::wstring res = L"[";
-        if (Dll) res += L"Dll,";
-        if (Exe) res += L"Exe,";
-        if (Script) res += L"Script,";
-        if (SourceFile) res += L"SourceFile,";
-        if (Directory) res += L"Directory,";
-        if (Continue) res += L"Continue,";
-        if (Async) res += L"Async,";
-        if (Rollback) res += L"Rollback,";
-        if (Commit) res += L"Commit,";
-        if (Deferred) res += L"Deferred,";
-        if (NoImpersonate) res += L"NoImpersonate,";
-        if (Script64Bit) res += L"Script64Bit,";
-        if (HideTarget) res += L"HideTarget,";
-        if (TSAware) res += L"TSAware,";
-        if (PatchUninstall) res += L"PatchUninstall,";
-        return res.substr(0, res.size() - 1) + L"]";
-    }
-
-    operator int& () {
-        return *reinterpret_cast<int*>(this);
-    }
-    operator const int& () const {
-        return *reinterpret_cast<const int*>(this);
-    }
-
-    /** Special combinations:
-    *   msidbCustomActionTypeTextData (0x03) = Dll | Exe 
-    *   msidbCustomActionTypeJScript (0x05) = 0x04 | Dll
-    *   msidbCustomActionTypeVBScript (0x06) = 0x04 | Dll
-    *   msidbCustomActionTypeInstall (0x07) = 0x04 | Exe | Dll
-    *   msidbCustomActionTypeProperty (0x30) = Directory | File
-    *   msidbCustomActionTypeClientRepeat (0x300) = FirstSequence + OncePerProcess */
-    bool Dll           : 1; ///< msidbCustomActionTypeDll (0x01)
-    bool Exe           : 1; ///< msidbCustomActionTypeExe (0x02)
-    bool Script        : 1; ///< script (used by msidbCustomActionTypeJScript (0x05) and msidbCustomActionTypeVBScript (0x06))
-    bool _padding1     : 1;
-    bool SourceFile    : 1; ///< msidbCustomActionTypeSourceFile (0x10)
-    bool Directory     : 1; ///< msidbCustomActionTypeDirectory (0x20)
-    bool Continue      : 1; ///< msidbCustomActionTypeContinue (0x40)
-    bool Async         : 1; ///< msidbCustomActionTypeAsync (0x80)
-    bool Rollback      : 1; ///< msidbCustomActionTypeFirstSequence or msidbCustomActionTypeRollback (0x100)
-    bool Commit        : 1; ///< msidbCustomActionTypeOncePerProcess or msidbCustomActionTypeCommit (0x200)
-    bool Deferred      : 1; ///< msidbCustomActionTypeInScript (0x400) (deferred execution)
-    bool NoImpersonate : 1; ///< msidbCustomActionTypeNoImpersonate (0x800) - run as ADMIN
-    bool Script64Bit   : 1; ///< msidbCustomActionType64BitScript (0x1000)
-    bool HideTarget    : 1; ///< msidbCustomActionTypeHideTarget (0x2000)
-    bool TSAware       : 1; ///< msidbCustomActionTypeTSAware (0x4000) (Terminal Server)
-    bool PatchUninstall: 1; ///< msidbCustomActionTypePatchUninstall (0x8000)
-    bool _padding2     : 8;
-    bool _padding3     : 8;
-};
-static_assert(sizeof(CustomActionType) == sizeof(int), "CustomActionType size mismatch");
-
 /** https://docs.microsoft.com/en-us/windows/win32/msi/customaction-table */
 struct CustomActionEntry {
-    std::wstring     Action;
-    CustomActionType Type;
-    std::wstring     Source;
-    std::wstring     Target;
-    std::wstring     ExtendedType;
+    /** CustomAction type parser.
+    *   Based on msidbCustomActionType enum in <msidefs.h>
+    *   REF: https://docs.microsoft.com/en-us/windows/win32/msi/summary-list-of-all-custom-action-types
+    *
+    *   Wix custom actions:
+    *    Type 65   (0x041)  =                                                                Continue(0x40)                    + Dll(0x01) // ValidatePath, PrintEula
+    *   Custom DLL:
+    *    Type               =                                                                                                  + Dll(0x01) // Type 1/17 run DLL
+    *   Custom EXE:
+    *    Type 3106 (0x0C22) =                       NoImpersonate(0x800) + Deferred(0x400)                  + Directory(0x20)  + Exe(0x02) // Type 2/18/34/50 run executable
+    *    Type 3170 (0x0C62) =                       NoImpersonate(0x800) + Deferred(0x400) + Continue(0x40) + Directory(0x20)  + Exe(0x02)
+    *   Custom JScript:
+    *    Type 7189 (0x1C15) = Script64Bit(0x1000) + NoImpersonate(0x800) + Deferred(0x400) +                  SourceFile(0x10) + Script(0x04) + Dll(0x01) // Type 5/21/37/53 JScript
+    *    Type 7253 (0x1C55) = Script64Bit(0x1000) + NoImpersonate(0x800) + Deferred(0x400) + Continue(0x40) + SourceFile(0x10) + Script(0x04) + Dll(0x01)
+    *   Custom VBScript:
+    *    Type               =                                                                                                  + Script(0x04) + Exe(0x02) // Type 6/22/38/54 VBScript
+    */
+    struct Type {
+        Type() {
+            memset(this, 0, sizeof(Type)); // replace with default member initializers after upgrading to newer C++ version
+        }
+
+        /** Parse MSI CustomAction "Type" column. */
+        Type(int val) {
+            (int&)(*this) = val;
+        }
+
+        std::wstring ToString() const {
+            std::wstring res = L"[";
+            if (Dll) res += L"Dll,";
+            if (Exe) res += L"Exe,";
+            if (Script) res += L"Script,";
+            if (SourceFile) res += L"SourceFile,";
+            if (Directory) res += L"Directory,";
+            if (Continue) res += L"Continue,";
+            if (Async) res += L"Async,";
+            if (Rollback) res += L"Rollback,";
+            if (Commit) res += L"Commit,";
+            if (Deferred) res += L"Deferred,";
+            if (NoImpersonate) res += L"NoImpersonate,";
+            if (Script64Bit) res += L"Script64Bit,";
+            if (HideTarget) res += L"HideTarget,";
+            if (TSAware) res += L"TSAware,";
+            if (PatchUninstall) res += L"PatchUninstall,";
+            return res.substr(0, res.size() - 1) + L"]";
+        }
+
+        operator int& () {
+            return *reinterpret_cast<int*>(this);
+        }
+        operator const int& () const {
+            return *reinterpret_cast<const int*>(this);
+        }
+
+        /** Special combinations:
+        *   msidbCustomActionTypeTextData (0x03) = Dll | Exe
+        *   msidbCustomActionTypeJScript (0x05) = 0x04 | Dll
+        *   msidbCustomActionTypeVBScript (0x06) = 0x04 | Dll
+        *   msidbCustomActionTypeInstall (0x07) = 0x04 | Exe | Dll
+        *   msidbCustomActionTypeProperty (0x30) = Directory | File
+        *   msidbCustomActionTypeClientRepeat (0x300) = FirstSequence + OncePerProcess */
+        bool Dll : 1; ///< msidbCustomActionTypeDll (0x01)
+        bool Exe : 1; ///< msidbCustomActionTypeExe (0x02)
+        bool Script : 1; ///< script (used by msidbCustomActionTypeJScript (0x05) and msidbCustomActionTypeVBScript (0x06))
+        bool _padding1 : 1;
+        bool SourceFile : 1; ///< msidbCustomActionTypeSourceFile (0x10)
+        bool Directory : 1; ///< msidbCustomActionTypeDirectory (0x20)
+        bool Continue : 1; ///< msidbCustomActionTypeContinue (0x40)
+        bool Async : 1; ///< msidbCustomActionTypeAsync (0x80)
+        bool Rollback : 1; ///< msidbCustomActionTypeFirstSequence or msidbCustomActionTypeRollback (0x100)
+        bool Commit : 1; ///< msidbCustomActionTypeOncePerProcess or msidbCustomActionTypeCommit (0x200)
+        bool Deferred : 1; ///< msidbCustomActionTypeInScript (0x400) (deferred execution)
+        bool NoImpersonate : 1; ///< msidbCustomActionTypeNoImpersonate (0x800) - run as ADMIN
+        bool Script64Bit : 1; ///< msidbCustomActionType64BitScript (0x1000)
+        bool HideTarget : 1; ///< msidbCustomActionTypeHideTarget (0x2000)
+        bool TSAware : 1; ///< msidbCustomActionTypeTSAware (0x4000) (Terminal Server)
+        bool PatchUninstall : 1; ///< msidbCustomActionTypePatchUninstall (0x8000)
+        bool _padding2 : 8;
+        bool _padding3 : 8;
+    };
+    static_assert(sizeof(Type) == sizeof(int), "CustomAction::Type size mismatch");
+
+
+    std::wstring Action;
+    Type         Type;
+    std::wstring Source;
+    std::wstring Target;
+    std::wstring ExtendedType;
 };
 
 
