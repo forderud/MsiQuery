c2ec4af95a9deb639c3d671608f208c4102fd01d
Fredrik Orderud <fredrik.orderud@ge.com>
2023-08-15 15:50:17 +0200

    Start implementing MSIX query.

--- MsixQuery/Program.cs
+++ MsixQuery/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Windows.ApplicationModel;
+using Windows.Management.Deployment;
+
+void GetManagementPackages()
+{
+    var mgr = new PackageManager();
+    var pkgs = mgr.FindPackagesForUser("");
+
+    foreach (Package pkg in pkgs)
+    {
+        Console.WriteLine("Package info:");
+        Console.WriteLine("  DisplayName: "+pkg.DisplayName);
+        Console.WriteLine("  Id: " + pkg.Id.FullName);
+        var ver = pkg.Id.Version;
+        Console.WriteLine("  Version: " + ver.Major+"."+ver.Minor+"."+ver.Revision+"."+ver.Build);
+    }
+}
+
+GetManagementPackages();
