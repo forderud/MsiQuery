c5788fe4de71a17b5905bdf41d10abb1bb0fd7b9
Fredrik Orderud <fredrik.orderud@ge.com>
2023-08-16 11:17:33 +0200

    Update Linq syntax.

--- MsixQuery/Program.cs
+++ MsixQuery/Program.cs
@@ -8,9 +8,9 @@ void GetManagementPackages()
     //var pkgs = mgr.FindPackages(); // require admin privileges
 
     // filter out system packages (not shown in "Apps & features")
-    pkgs = pkgs.Where(elm => elm.SignatureKind != PackageSignatureKind.System).ToList();
+    pkgs = pkgs.Where(_ => _.SignatureKind != PackageSignatureKind.System).ToList();
     // filter out frameworks (not shown in "Apps & features")
-    pkgs = pkgs.Where(elm => elm.IsFramework == false).ToList();
+    pkgs = pkgs.Where(_ => _.IsFramework == false).ToList();
 
     Console.WriteLine($"Found {pkgs.Count()} packages.");
 
