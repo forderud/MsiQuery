d2c8bfed0fcffab41b3a8a5735c2e24a8e70e072
Fredrik Orderud <fredrik.orderud@ge.com>
2023-08-30 22:09:55 +0200

    Also print UpgradeCode if present.

--- AppDetect.ps1
+++ AppDetect.ps1
@@ -10,8 +10,13 @@ $apps = $apps | Where-Object {
 # List MSI- and EXE-installed apps
 foreach($app in $apps)
 {
+    # check if app has UpgradeCode
+    $query = "SELECT Value FROM Win32_Property WHERE Property='UpgradeCode' AND ProductCode='"+$app.PSChildName+"'"
+    $UpgradeCode = gwmi -Query $query
+    
     Write-Host ""
     Write-Host ID: $app.PSChildName
+    Write-Host UpgradeCode: $UpgradeCode.Value
     Write-Host DisplayName: $app.DisplayName
     Write-Host DisplayVersion: $app.DisplayVersion
     Write-Host Publisher: $app.Publisher
